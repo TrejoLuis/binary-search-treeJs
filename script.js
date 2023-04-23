29d11f7235b72761bebbc2ba9ecdb8b93e412288
Luis Trejo <trejorias@gmail.com>
2023-04-23 11:42:19 -0600

    Feat: add Node and Tree class, buildTree fn which turns an array into a balanced binary tree

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log('HELLO')
+
+class Node {
+  constructor(data){
+    this.data = data
+    this.left = null
+    this.right = null
+  }
+}
+
+class Tree {
+  constructor(array){
+    this.root = buildTree(array)
+  }
+
+}
+
+function buildTree(array, start=0, end=array.length-1){
+  //base case
+  if(start > end) return null
+
+  const mid = parseInt((start+end)/2)
+  const node = new Node(array[mid]) 
+
+  node.left = buildTree(array,start,mid-1)
+  node.rigth = buildTree(array,mid+1,end)
+
+  return node 
+}
